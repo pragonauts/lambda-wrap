a3cf1391cba838b4919c308c1a99dde73f3f6d38
Marek Takac <marek.takac@nejremeslnici.cz>
2017-12-19 11:55:58 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const wrapError = require('./lib/error');
+const { LambdaWrap } = require('./lib/lambda-wrap');
+
+module.exports = { LambdaWrap, wrapError };
---
+++ test/lib/lambda-wrap.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { assert } = require('chai');
+
+describe('LambdaWrap', () => {
+    describe('new', () => {
+        it('does something', () => {
+            assert.equal(2, 1 + 1);
+        });
+    });
+});
